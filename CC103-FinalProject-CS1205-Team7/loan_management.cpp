698d376a90ad09a28f6cd696de59d67de854861c
Cenen <socitocenen3@gmail.com>
2026-04-05 00:02:16 +0800

    first commit

---
+++ CC103-FinalProject-CS1205-Team7/loan_management.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Welcome to the Loan Management System!" << endl;
+    // Additional code for loan management will go here.
+    return 0;
+}
