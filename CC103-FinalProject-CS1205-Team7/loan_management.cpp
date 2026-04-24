7df995803d57dfbf49171c3beaac394448681d5f
mgl-lgz <lagazom1@gmail.com>
2026-04-25 00:04:34 +0800

    Add files via upload
    "I added the Due function in a void data type that accesses the current time and the time after a month"

--- CC103-FinalProject-CS1205-Team7/loan_management.cpp
+++ CC103-FinalProject-CS1205-Team7/loan_management.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <string>
+#include <ctime>
+#include <chrono>
 using namespace std;
 
+void Due(time_t now);
+
+
 int main()
-{
+{   time_t now = time(0);
+
     cout << "Welcome to the Loan Management System!" << endl;
     // Additional code for loan management will go here.
+
+    Due(now);
+
     return 0;
+}
+
+//for the 1 month due date
+void Due(time_t now) {
+    time_t due = now + (30 * 24 * 60 * 60);
+
+    cout << "Current date: " << ctime(&now);
+    cout << "Due date (30 days): " << ctime(&due);
 }
